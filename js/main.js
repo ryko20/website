c159d8597d3a886196a9b22f6da3a93cce76dc74
ryan-kobayashi <ryan.s.kobayashi@gmail.com>
2025-01-24 21:10:33 -0500

    Remove excess, edit and add css, abt page, and icon

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("components/navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar-placeholder").innerHTML = data;
-        });
-});
+function addComponent(component) {
+  console.log(`Adding component: ${component}`);
+
+  document.addEventListener("DOMContentLoaded", () => {
+    fetch(`components/${component}.html`)
+      .then((response) => response.text())
+      .then((data) => {
+        const placeholder = document.getElementById(`${component}-placeholder`);
+        if (placeholder) {
+          placeholder.innerHTML = data;
+        } else {
+          console.error(
+            `Element with ID '${component}-placeholder' not found.`
+          );
+        }
+      });
+  });
+}
+
+addComponent("navbar");
